903beb33547dce124ee32a5dcf75a7d140de9061
Saeid Mohammadi <saeid.mhd@gmail.com>
2023-12-31 12:13:07 +0330

    add glide to show poster

--- app/src/main/java/com/saeidmohammadi/batman/MovieAdapter.kt
+++ app/src/main/java/com/saeidmohammadi/batman/MovieAdapter.kt
@@ -3,8 +3,12 @@ package com.saeidmohammadi.batman
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 
 class MovieAdapter(private val onItemClick: (Movie) -> Unit) :
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -36,7 +40,24 @@ class MovieAdapter(private val onItemClick: (Movie) -> Unit) :
             itemView.findViewById<TextView>(R.id.titleTextView).text = movie.Title
             itemView.findViewById<TextView>(R.id.yearTextView).text = movie.Year
             itemView.findViewById<TextView>(R.id.typeTextView).text = movie.Type
-            // اضافه کردن کتابخانه تصاویر (مانند Glide) برای نمایش پوستر فیلم
+
+            // ImageView برای نمایش پوستر فیلم
+            val posterImageView: ImageView = itemView.findViewById(R.id.posterImageView)
+
+            // آدرس پوستر فیلم
+            val posterUrl = movie.Poster
+
+            // تنظیمات بارگذاری Glide
+            val requestOptions = RequestOptions()
+                .placeholder(R.drawable.ic_launcher_foreground) // تصویر پیش‌فرض برای نمایش تا زمانی که تصویر فیلم بارگذاری نشده است
+                .error(R.drawable.ic_launcher_background) // تصویر نشان داده می‌شود در صورتی که بارگذاری تصویر با مشکل مواجه شود
+
+            // بارگذاری تصویر با استفاده از Glide
+            Glide.with(itemView.context)
+                .load(posterUrl)
+                .apply(requestOptions)
+                .transition(DrawableTransitionOptions.withCrossFade()) // افکت انتقال تصویر
+                .into(posterImageView)
         }
     }
 }
